4c529336793680f24740f260ebe5b8d6316a8a38
S4chetti <kivancisaozdemir35@gmail.com>
2025-11-10 13:43:31 +0300

    Internet ProgramciligiAdd project files.

--- IPD/Controllers/HomeController.cs
+++ IPD/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace IPD.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Test1()
+        {
+            return View();
+        }
+    
     }
 }
