2628924a45b70866197b7bef20f48d8d723de214
S4chetti <kivancisaozdemir35@gmail.com>
2025-12-24 03:06:54 +0300

    Açıklamalar kısmı düzenlendi

--- Forum.Entity/Models/Comment.cs
+++ Forum.Entity/Models/Comment.cs
@@ -1,10 +1,18 @@
-﻿namespace Forum.Entity.Models
+﻿using System; // DateTime için bu gerekli
+using System.ComponentModel.DataAnnotations;
+
+namespace Forum.Entity.Models
 {
     public class Comment
     {
         public int Id { get; set; }
+
+        [Required]
         public string Content { get; set; }
+
+        // CreatedDate BURADA OLMALI (Sınıfın parantezleri içinde)
         public DateTime CreatedDate { get; set; } = DateTime.Now;
+
         public int QuestionId { get; set; }
         public Question Question { get; set; }
 
---
+++ Forum.Web/Controllers/CommentController.cs
@@ -0,0 +1,49 @@
+﻿using Forum.Data;
+using Forum.Entity.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Forum.Web.Controllers
+{
+    [Authorize] // Sadece giriş yapanlar yorum atabilir
+    public class CommentController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public CommentController(AppDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Comment comment)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                // Kullanıcı yoksa JSON olarak hata dön
+                return Json(new { success = false, message = "Oturum açmalısınız." });
+            }
+
+            comment.UserId = user.Id;
+            comment.CreatedDate = DateTime.Now;
+
+            _context.Comments.Add(comment);
+            await _context.SaveChangesAsync();
+
+            // Sayfayı yenilemek yerine, eklenen veriyi JSON olarak geri yolluyoruz
+            return Json(new
+            {
+                success = true,
+                userName = user.Name ?? user.UserName, // Ad yoksa kullanıcı adı
+                date = comment.CreatedDate.ToString("dd.MM.yyyy HH:mm"),
+                content = comment.Content
+            });
+        }
+    }
+}
